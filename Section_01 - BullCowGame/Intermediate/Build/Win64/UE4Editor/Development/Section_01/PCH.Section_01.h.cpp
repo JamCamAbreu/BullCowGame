9905fbfae130f5964ac7603135d9ac4724a108dc
James Cameron Abreu <coolcam222@gmail.com>
2016-08-25 15:54:28 -0600

    Import BullCow game files

---
+++ Section_01 - BullCowGame/Intermediate/Build/Win64/UE4Editor/Development/Section_01/PCH.Section_01.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\Users\James Cam Abreu\Documents\Unreal Projects\Section_01\Source\Section_01\Section_01.h"
---
+++ Section_01 - BullCowGame/Intermediate/Build/Win64/UE4Editor/Development/Section_01/Section_01.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_SECTION_01_H__
+#define __AUTO_SECTION_01_H__
+//Last Write: 8/17/2016 6:41:01 PM +00:00
+#include "C:\Users\James Cam Abreu\Documents\Unreal Projects\Section_01\Source\Section_01\Section_01.h"
+#endif//__AUTO_SECTION_01_H__
